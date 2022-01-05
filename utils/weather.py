4555ba94a18d8c655633d07fa8c3a3c1a4c8b620
annnvv <13822260+annnvv@users.noreply.github.com>
2022-01-04 21:14:10 -0500

    removed tkinter image reference to utils/display.py and added TODOs

--- utils/weather.py
+++ utils/weather.py
@@ -2,51 +2,37 @@ import requests
 import json
 from datetime import datetime
 
-# maps open weather icons to weather conditions
-# https://openweathermap.org/weather-conditions
-icon_lookup = {
-    "thunderstorm": "imgs/Storm.png",  # thunderstorm
-    "drizzle": "imgs/Rain.png",  # rain day
-    "rain": "imgs/Rain.png",  # rain day
-    "snow": "imgs/Snow.png",  # snow day
-    "mist": "imgs/Haze.png",
-    "smoke": "imgs/Haze.png",
-    "haze": "imgs/Haze.png",
-    "dust": "imgs/Haze.png",
-    "fog": "imgs/Haze.png",  # fog day
-    "sand": "imgs/Haze.png",
-    "ash": "imgs/Haze.png",
-    "squall": "imgs/Haze.png",
-    "tornado": "imgs/Tornado.png",  # tornado
-    "clear-sky": "imgs/Sun.png",  # clear sky day
-    "clear-night": "imgs/Moon.png",  # clear sky night
-    "clouds": "imgs/Cloud.png",  # cloudy day
-    "partly-cloudy-day": "imgs/PartlySunny.png",  # partly cloudy day
-    "partly-cloudy-night": "imgs/PartlyMoon.png",  # scattered clouds night
-    # 'wind': 'imgs/Wind.png',  # wind
-    # 'snow-thin': 'imgs/Snow.png',  # sleet day
-    # 'hail': 'imgs/Hail.png',  # hail
-}
-
 
 class Weather:
     def __init__(self):
-        self.temp = ""
-        self.feels_like = ""
-        self.temp_max = ""
-        self.temp_min = ""
-        self.weather_desc = ""
-        self.sunrise = ""
-        self.sunset = ""
+        # self.temp = ""
+        # self.feels_like = ""
+        # self.temp_max = ""
+        # self.temp_min = ""
+        # self.weather_desc = ""
+        # self.sunrise = ""
+        # self.sunset = ""
+        self.zip_code = ""
+        self.weather_api_key = ""
 
-    def get_weather(self, zip_code: str, weather_api_key: str):
+    ##TODO: Figure out how to pass authentication!
+    ##TODO: Figure out how to implement tkinter after method
+    @classmethod
+    def get_weather(cls) -> None:
         """
         Method to query the openweathermap API to get current temperature, feels like temperature,
         daily min temperature, daily max temperature, weather conditions, sunrise, and sunset
+
+        Args:
+            zip_code (str): zipcode
+            weather_api_key (str): openweathermap API key
+        Returns:
+            None
         """
 
+        ## Make GET request
         weather_req_url = str(
-            f"https://api.openweathermap.org/data/2.5/weather?zip={zip_code}&appid={weather_api_key}&units=imperial"
+            f"https://api.openweathermap.org/data/2.5/weather?zip={cls.zip_code}&appid={cls.weather_api_key}&units=imperial"
         )
 
         try:
@@ -56,38 +42,20 @@ class Weather:
         except requests.exceptions.RequestException as e:
             print("Raise error:", e)
 
-        # degree_sign = "\N{DEGREE SIGN}"
+        ## Parse data from request
+        cls.temp = str(round(weather_obj["main"]["temp"]))
+        cls.feels_like = str(round(weather_obj["main"]["feels_like"]))
+        cls.temp_max = str(round(weather_obj["main"]["temp_max"]))
+        cls.temp_min = str(round(weather_obj["main"]["temp_min"]))
 
-        self.temp = str(round(weather_obj["main"]["temp"]))
-        self.feels_like = str(round(weather_obj["main"]["feels_like"]))
-        self.temp_max = str(round(weather_obj["main"]["temp_max"]))
-        self.temp_min = str(round(weather_obj["main"]["temp_min"]))
-        self.weather_desc = weather_obj["weather"][0]["main"]
-        self.sunrise = datetime.fromtimestamp(weather_obj["sys"]["sunrise"]).strftime(
+        cls.weather_desc = weather_obj["weather"][0]["main"]
+
+        cls.sunrise = datetime.fromtimestamp(weather_obj["sys"]["sunrise"]).strftime(
             "%H:%M"
         )
-        self.sunset = datetime.fromtimestamp(weather_obj["sys"]["sunset"]).strftime(
+        cls.sunset = datetime.fromtimestamp(weather_obj["sys"]["sunset"]).strftime(
             "%H:%M"
         )
+        return None
 
-        #         icon_id = weather_obj['currently']['icon']
-        #         icon2 = None
-
-        #         if icon_id in icon_lookup:
-        #             icon2 = icon_lookup[icon_id]
-
-        #         if icon2 is not None:
-        #             if self.icon != icon2:
-        #                 self.icon = icon2
-        #                 image = Image.open(icon2)
-        #                 image = image.resize((100, 100), Image.ANTIALIAS)
-        #                 image = image.convert('RGB')
-        #                 photo = ImageTk.PhotoImage(image)
-
-        #                 self.iconLbl.config(image=photo)
-        #                 self.iconLbl.image = photo
-        #         else:
-        #             # remove image
-        #             self.iconLbl.config(image='')
-
-        # self.after(600000 * 3, self.get_weather)
+    # self.after(600000 * 3, self.get_weather)
