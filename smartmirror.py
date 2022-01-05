61b366760616cc94516c924fa1d9090813ed0ce5
annnvv <13822260+annnvv@users.noreply.github.com>
2022-01-04 21:54:35 -0500

    removed unnecessary import statements

--- smartmirror.py
+++ smartmirror.py
@@ -1,16 +1,12 @@
 # smartmirror.py
 
-# from tkinter import *
 from utils.display import displayWindow
-from utils import text, read_yaml
 from utils.weather import Weather
 
 # from utils.train import Wmata
 # from utils.clock import Clock
 
-config = read_yaml()
-
-weather = Weather()
+# weather = Weather()
 # train = Wmata()
 
 w = displayWindow()
