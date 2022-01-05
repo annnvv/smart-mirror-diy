5e659d7355daad4497adf0b2db032be81943acf2
annnvv <13822260+annnvv@users.noreply.github.com>
2022-01-04 21:53:26 -0500

    updated function to a class

--- utils/read_yaml.py
+++ utils/read_yaml.py
@@ -1,6 +1,16 @@
 import yaml
 
 
-def read_yaml(file_path):
-    with open(file_path, "r") as f:
-        return yaml.safe_load(f)
+class configData:
+    def __init__(self) -> None:
+        with open("../_auth/config.yaml", "r") as f:
+            config = yaml.safe_load(f)
+
+        self.weather_api_key = config["weather"]["api_key"]
+        self.weather_zip_code = config["weather"]["zip_code"]
+
+        self.wmata_api_key = config["wmata"]["api_key"]
+        self.wmata_station_code = config["wmata"]["station_code"]
+
+        self.dash_bus_number = config["dash"]["bus_number"]
+        self.dash_stop_numbers = config["dash"]["stop_numbers"]
