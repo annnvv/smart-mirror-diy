1c60441b3f0b0f0d8890ccb1c4edf8663ad40891
annnvv <13822260+annnvv@users.noreply.github.com>
2022-01-08 17:31:40 -0500

    added class inheritance

--- utils/weather.py
+++ utils/weather.py
@@ -1,38 +1,25 @@
 import requests
 import json
 from datetime import datetime
+from utils.read_yaml import configData
 
 
-class Weather:
-    def __init__(self):
-        # self.temp = ""
-        # self.feels_like = ""
-        # self.temp_max = ""
-        # self.temp_min = ""
-        # self.weather_desc = ""
-        # self.sunrise = ""
-        # self.sunset = ""
-        self.zip_code = ""
-        self.weather_api_key = ""
-
-    ##TODO: Figure out how to pass authentication!
+class Weather(configData):
     ##TODO: Figure out how to implement tkinter after method
-    @classmethod
-    def get_weather(cls) -> None:
+    def get_weather(self) -> None:
         """
         Method to query the openweathermap API to get current temperature, feels like temperature,
         daily min temperature, daily max temperature, weather conditions, sunrise, and sunset
 
         Args:
-            zip_code (str): zipcode
-            weather_api_key (str): openweathermap API key
+            None, weather_zip_code and weather_api_key are inherited from configData
         Returns:
             None
         """
 
         ## Make GET request
         weather_req_url = str(
-            f"https://api.openweathermap.org/data/2.5/weather?zip={cls.zip_code}&appid={cls.weather_api_key}&units=imperial"
+            f"https://api.openweathermap.org/data/2.5/weather?zip={self.weather_zip_code}&appid={self.weather_api_key}&units=imperial"
         )
 
         try:
@@ -43,17 +30,17 @@ class Weather:
             print("Raise error:", e)
 
         ## Parse data from request
-        cls.temp = str(round(weather_obj["main"]["temp"]))
-        cls.feels_like = str(round(weather_obj["main"]["feels_like"]))
-        cls.temp_max = str(round(weather_obj["main"]["temp_max"]))
-        cls.temp_min = str(round(weather_obj["main"]["temp_min"]))
+        self.temp = str(round(weather_obj["main"]["temp"]))
+        self.feels_like = str(round(weather_obj["main"]["feels_like"]))
+        self.temp_max = str(round(weather_obj["main"]["temp_max"]))
+        self.temp_min = str(round(weather_obj["main"]["temp_min"]))
 
-        cls.weather_desc = weather_obj["weather"][0]["main"]
+        self.weather_desc = weather_obj["weather"][0]["main"]
 
-        cls.sunrise = datetime.fromtimestamp(weather_obj["sys"]["sunrise"]).strftime(
+        self.sunrise = datetime.fromtimestamp(weather_obj["sys"]["sunrise"]).strftime(
             "%H:%M"
         )
-        cls.sunset = datetime.fromtimestamp(weather_obj["sys"]["sunset"]).strftime(
+        self.sunset = datetime.fromtimestamp(weather_obj["sys"]["sunset"]).strftime(
             "%H:%M"
         )
         return None
