e8e1ae06d481967e42f0116ec1084ea2bffcbdfb
annnvv <13822260+annnvv@users.noreply.github.com>
2022-01-14 02:19:41 -0500

    re-structured train_info into a nested list and added code for a station open close method

--- utils/train.py
+++ utils/train.py
@@ -1,20 +1,42 @@
+from datetime import datetime
 import requests
 import json
-from utils.read_yaml import configData
+from read_yaml import configData
 
 
 class Wmata(configData):
-    def get_next_train(self):
+    # def get_station_open_close(self) -> None:
+    #     """Get station information open and close times"""
+    #     wmata_req_url = str(
+    #         f"https://api.wmata.com/Rail.svc/json/jStationTimes/{self.wmata_station_code}"
+    #     )
+    #     headers = {
+    #         "api_key": self.wmata_api_key,
+    #     }
+    #     try:
+    #         r = requests.get(wmata_req_url, params=headers)
+    #         self.station_obj = json.loads(r.text)
+
+    #     except requests.exceptions.RequestException as e:
+    #         print("Error:", e)
+
+    #     return None
+
+    ##TODO2: limit requests only during when the train is running
+    ##TODO3: in order to implement above, need to
+    ##https://www.wmata.com/rider-guide/stations/van-dorn-st.cfm
+
+    def get_next_train(self) -> None:
         """
         Method that queries the WMATA API to get information about the next train(s) arriving at a station
         Args:
             None; wmata_station_code and wmata_api_key are inherited from configData
         Returns:
-            A list of strings that contain information about the Line, Destination, and Minutes to next train
+            Technically returns None,
+            Class has an class variable with a nested list of strings that contain information about the Line, Destination, and Minutes to next train
         """
 
         ## Make GET request
-        ##TODO: limit requests only during when the train is running
         wmata_req_url = str(
             f"https://api.wmata.com/StationPrediction.svc/json/GetPrediction/{self.wmata_station_code}"
         )
@@ -30,16 +52,15 @@ class Wmata(configData):
             print("Error:", e)
 
         ## Parse data from request
-        ##TODO: Figure out a different data structure for this data, maybe dictionary?
-        train_info = []
-        for i in range(len(train_obj["Trains"])):
-            train = (
-                train_obj["Trains"][i]["Line"]
-                + " "
-                + train_obj["Trains"][i]["Destination"]
-                + " "
-                + train_obj["Trains"][i]["Min"]
-            )
-            train_info.append(train)
+        ##TODO1: maybe implement this as a dictionary rather than a nested list???
+        self.train_info = []
+        if train_obj["Trains"]:
+            for i in range(len(train_obj["Trains"])):
+                train = [
+                    [train_obj["Trains"][i]["Line"]],
+                    [train_obj["Trains"][i]["Destination"]],
+                    [train_obj["Trains"][i]["Min"]],
+                ]
+                self.train_info.append(train)
 
-        return train_info
+        return None
