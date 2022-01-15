1af857d030db00d604e776f2e1532c45a48862f0
annnvv <13822260+annnvv@users.noreply.github.com>
2022-01-14 20:29:45 -0500

    added conditional logic to run the api query between 07AM and min till midnight

--- utils/train.py
+++ utils/train.py
@@ -5,6 +5,9 @@ from read_yaml import configData
 
 
 class Wmata(configData):
+    ##TODO3: in order to implement TODO2, need to get station first and last train (API currently not working)
+    ##https://www.wmata.com/rider-guide/stations/van-dorn-st.cfm
+
     # def get_station_open_close(self) -> None:
     #     """Get station information open and close times"""
     #     wmata_req_url = str(
@@ -22,10 +25,6 @@ class Wmata(configData):
 
     #     return None
 
-    ##TODO2: limit requests only during when the train is running
-    ##TODO3: in order to implement above, need to
-    ##https://www.wmata.com/rider-guide/stations/van-dorn-st.cfm
-
     def get_next_train(self) -> None:
         """
         Method that queries the WMATA API to get information about the next train(s) arriving at a station
@@ -36,7 +35,7 @@ class Wmata(configData):
             Class has an class variable with a nested list of strings that contain information about the Line, Destination, and Minutes to next train
         """
 
-        ## Make GET request
+        ## Make GET request (only when metro station is open)
         wmata_req_url = str(
             f"https://api.wmata.com/StationPrediction.svc/json/GetPrediction/{self.wmata_station_code}"
         )
@@ -44,23 +43,27 @@ class Wmata(configData):
             "api_key": self.wmata_api_key,
         }
 
-        try:
-            r = requests.get(wmata_req_url, params=headers)
-            train_obj = json.loads(r.text)
+        ##TODO2: (medium priority) limit requests only during when the train is running (note to self: this might be more appropriate in the display/GUI than here)
+        current_time = datetime.now().strftime("%H:%M")
+        if current_time > "07:00" and current_time < "23:59":
+            print(current_time)
+            try:
 
-        except requests.exceptions.RequestException as e:
-            print("Error:", e)
+                r = requests.get(wmata_req_url, params=headers)
+                train_obj = json.loads(r.text)
 
-        ## Parse data from request
-        ##TODO1: maybe implement this as a dictionary rather than a nested list???
-        self.train_info = []
-        if train_obj["Trains"]:
-            for i in range(len(train_obj["Trains"])):
-                train = [
-                    [train_obj["Trains"][i]["Line"]],
-                    [train_obj["Trains"][i]["Destination"]],
-                    [train_obj["Trains"][i]["Min"]],
-                ]
-                self.train_info.append(train)
+            except requests.exceptions.RequestException as e:
+                print("Error:", e)
 
+            ## Parse data from request
+            ##TODO1: (low priority) maybe implement this as a dictionary rather than a nested list???
+            self.train_info = []
+            if train_obj["Trains"]:
+                for i in range(len(train_obj["Trains"])):
+                    train = [
+                        [train_obj["Trains"][i]["Line"]],
+                        [train_obj["Trains"][i]["Destination"]],
+                        [train_obj["Trains"][i]["Min"]],
+                    ]
+                    self.train_info.append(train)
         return None
