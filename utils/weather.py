40a23304208900c37b105e8830c295ac76d10c40
annnvv <13822260+annnvv@users.noreply.github.com>
2022-01-14 20:15:44 -0500

    added image path attribute

--- utils/weather.py
+++ utils/weather.py
@@ -5,6 +5,31 @@ from read_yaml import configData
 
 
 class Weather(configData):
+
+    # maps open weather icons to weather conditions, see https://openweathermap.org/weather-conditions
+    ##TODO: (low priority)implement logic for clear day/night and cloudly day/night (something having to do with sunrise or sunset)
+    __icon_lookup = {
+        "clear": "imgs/Sun.png",
+        # "clear-sky": "imgs/Sun.png",  # clear sky day
+        # "clear-night": "imgs/Moon.png",  # clear sky night
+        "clouds": "imgs/Cloud.png",  # cloudy day
+        # "partly-cloudy-day": "imgs/PartlySunny.png",  # partly cloudy day
+        # "partly-cloudy-night": "imgs/PartlyMoon.png",  # scattered clouds night
+        "thunderstorm": "imgs/Storm.png",  # thunderstorm
+        "drizzle": "imgs/Rain.png",  # rain day
+        "rain": "imgs/Rain.png",  # rain day
+        "snow": "imgs/Snow.png",  # snow day
+        "mist": "imgs/Haze.png",
+        "smoke": "imgs/Haze.png",
+        "haze": "imgs/Haze.png",
+        "dust": "imgs/Haze.png",
+        "fog": "imgs/Haze.png",  # fog day
+        "sand": "imgs/Haze.png",
+        "ash": "imgs/Haze.png",
+        "squall": "imgs/Haze.png",
+        "tornado": "imgs/Tornado.png",  # tornado
+    }
+
     def get_weather(self) -> None:
         """
         Method to query the openweathermap API to get current temperature, feels like temperature,
@@ -29,13 +54,14 @@ class Weather(configData):
             print("Raise error:", e)
 
         ## Parse data from request
-        ##TODO: maybe created a different instance method to parse the data??
+        ##TODO: (low priority) maybe created a different instance method to parse the data??
         self.temp = str(round(weather_obj["main"]["temp"]))
         self.feels_like = str(round(weather_obj["main"]["feels_like"]))
         self.temp_max = str(round(weather_obj["main"]["temp_max"]))
         self.temp_min = str(round(weather_obj["main"]["temp_min"]))
 
         self.weather_desc = weather_obj["weather"][0]["main"]
+        self.img_path = self.__icon_lookup[self.weather_desc.lower()]
 
         self.sunrise = datetime.fromtimestamp(weather_obj["sys"]["sunrise"]).strftime(
             "%H:%M"
